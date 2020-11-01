c3263b713ab042b95da7d0141ef7743bffa1db13
Sergu9ary <sergunary.vs2@gmail.com>
2020-11-01 18:28:11 +0300

    Add new methods

--- src/com/company/Vector2D.java
+++ src/com/company/Vector2D.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.math.RoundingMode;
+import java.util.Locale;
+
 public class Vector2D {
     double a, b;
 
@@ -9,18 +12,43 @@ public class Vector2D {
         this.b = b;
 
     }
-    public Vector2D(Vector2D v){
+
+    public Vector2D(Vector2D v) {
         this.a = v.a;
         this.b = v.b;
 
     }
+
     public Vector2D() {
         this.a = 1.00;
         this.b = 1.00;
     }
 
+    public double length() {
+        double len = (Math.sqrt(this.a * this.a + this.b * this.b));
+        return len;
+    }
+
+    public Vector2D add(Vector2D v) {
+        return new Vector2D(this.a + v.a, this.b + v.b);
+
+    }
+
+    public Vector2D sub(Vector2D v) {
+        return new Vector2D(this.a + v.a, this.b + v.b);
+
+    }
+
+    public Vector2D scale(double scalefactor) {
+        return new Vector2D(this.a * scalefactor, this.b * scalefactor);
+
+    }
     public void print() {
-        System.out.println("(" + this.a + "," + this.b + ")");
+        System.out.print("(");
+        System.out.printf("%.2f", this.a);
+        System.out.print(", ");
+        System.out.printf("%.2f", this.b);
+        System.out.println(")");
 
     }
 }
